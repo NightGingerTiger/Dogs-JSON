6be07b28325a55800c5de7677e7bd3a9caab2fbb
Rozaliya <tiger.night.tiger@gmail.com>
2024-11-17 01:50:49 +0300

    Добавлен новый виджит nootebook

--- Dogs.py
+++ Dogs.py
@@ -30,9 +30,9 @@ def show_image():
             img.thumbnail(img_size)
             img = ImageTk.PhotoImage(img)
 
-            new_window = Toplevel(window)
-            new_window.title("Случайное изображение пёсика")
-            label = ttk.Label(new_window, image=img)
+            tab = ttk.Frame(notebook)
+            notebook.add(tab, text=f"Изображение № {notebook.index('end') + 1}")
+            label = ttk.Label(tab, image=img)
             label.image = img
             label.pack(padx=10, pady=10)
 
@@ -67,5 +67,11 @@ height_label = ttk.Label(text="Высота:")
 height_label.pack(side='left', padx=(10, 0))
 height_spinbox = ttk.Spinbox(from_=200, to=500, increment=50, width=5)
 height_spinbox.pack(side='left', padx=(0, 10))
+
+top_level_window = Toplevel(window)
+top_level_window.title("Изображения пёсиков")
+
+notebook = ttk.Notebook(top_level_window)
+notebook.pack(expand=True, fill='both', padx=10, pady=10)
 
 window.mainloop()
